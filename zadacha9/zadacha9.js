24108e782662741caa0bb10d93bb26028155ce40
YoanM <yoan.marinov.1@gmail.com>
2025-01-06 23:36:07 +0200

    spas, reshish 9ta

---
+++ zadacha9/zadacha9.js
@@ -0,0 +1,24 @@
+let duljina = 85;
+console.log(`vh1 - duljina = ${duljina} cm`);
+let shiro4ina = 75;
+console.log(`vh1 - shiro4ina = ${shiro4ina} cm`);
+let viso4ina = 47;
+console.log(`vh1 - viso4ina = ${viso4ina} cm`);
+let procent = 0.17;
+//---------------------------------------------------//
+let volume = (duljina * shiro4ina) * viso4ina / 1000;
+let volumeIprocent = volume - (volume * procent);
+console.log(`vhod1 = neobhodimi litri voda = ${volumeIprocent}`);
+
+let duljina1 = 105;
+console.log(`vh2 - duljina = ${duljina1} cm`);
+let shiro4ina1 = 77;
+console.log(`vh2 - shiro4ina = ${shiro4ina1} cm`);
+let viso4ina1 = 89;
+console.log(`vh2 - viso4ina = ${viso4ina1} cm`);
+let procent1 = 0.185;
+//---------------------------------------------------//
+let volume1 = (duljina1 * shiro4ina1) * viso4ina1 / 1000;
+let volumeIprocent1 = volume1 - (volume1 * procent1);
+console.log(`vhod2 = neobhodimi litri voda = ${volumeIprocent1}`);
+
