5cd59e3695b594a4e0a2f13372b8bc922df1f866
YoanM <129237730+Kurosl@users.noreply.github.com>
2025-01-07 20:50:16 +0200

    Update zadacha9.js
    opravih

--- zadacha9/zadacha9.js
+++ zadacha9/zadacha9.js
@@ -5,7 +5,7 @@ console.log(`vh1 - shiro4ina = ${shiro4ina} cm`);
 let viso4ina = 47;
 console.log(`vh1 - viso4ina = ${viso4ina} cm`);
 let procent = 0.17;
-//---------------------------------------------------//
+
 let volume = (duljina * shiro4ina) * viso4ina / 1000;
 let volumeIprocent = volume - (volume * procent);
 console.log(`vhod1 = neobhodimi litri voda = ${volumeIprocent}`);
@@ -17,8 +17,7 @@ console.log(`vh2 - shiro4ina = ${shiro4ina1} cm`);
 let viso4ina1 = 89;
 console.log(`vh2 - viso4ina = ${viso4ina1} cm`);
 let procent1 = 0.185;
-//---------------------------------------------------//
+
 let volume1 = (duljina1 * shiro4ina1) * viso4ina1 / 1000;
 let volumeIprocent1 = volume1 - (volume1 * procent1);
 console.log(`vhod2 = neobhodimi litri voda = ${volumeIprocent1}`);
-
